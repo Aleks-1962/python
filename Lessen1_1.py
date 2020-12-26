05ff6a8c0e31e603d51286e8e7c9d7caff4aa817
Aleks-1962 <ms_07_2012@mail.ru>
2020-12-26 08:58:01 +0400

    Initial commit

---
+++ Lessen1_1.py
@@ -0,0 +1,14 @@
+number_int = 125
+number_float = 125.00
+text_str = 'Привет'
+text_str2 = "Не совсем привет"
+is_no = False
+
+print(number_int, number_float, text_str, text_str2, is_no)
+
+seconds = int(input('Введите время в секундах: '))
+print(seconds)
+costs = float(input('Введите величину издержек тыс.руб '))
+print(costs)
+name = input('Ваше имя;')
+print(name)
